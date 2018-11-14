c2630dda23e21ffeb5cd389d603b81ecfedbd8c2
cdaringe <christopher.dieringer@gmail.com>
2018-11-14 00:48:13 -0800

    feat: support pretty date in post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,15 +1,17 @@
 import { graphql } from 'gatsby'
 import Helmet from 'react-helmet'
-import Layout from '../components/layout'
+import Layout from './Layout'
 import React from 'react'
 
 export default ({ data }) => {
   const post = data.markdownRemark
+  const { prettyDate, title } = post.frontmatter
   return (
     <Layout>
-      <Helmet title={`${post.frontmatter.title} - cdaringe - blog`} />
+      <Helmet title={`${title} - cdaringe - blog`} />
       <div>
-        <h1>{post.frontmatter.title}</h1>
+        <h1 style={{ marginBottom: 0 }}>{title}</h1>
+        <h6 style={{ margin: '0 0 0.6em 0', fontStyle: 'italic' }}>{prettyDate}</h6>
         <div className='markdown-body' dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
     </Layout>
@@ -21,6 +23,7 @@ export const query = graphql`
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
       frontmatter {
+        prettyDate: date(formatString: "MMMM D, YYYY", locale: "us")
         title
       }
     }
