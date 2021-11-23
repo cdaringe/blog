544d5bafb425fa2a171cc7c8d2bce9ab90e0ebde
cdaringe <cdaringe@gmail.com>
2021-11-23 15:33:19 -0800

    fix: lower node to 14 for arm support

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 set -euox pipefail
 
 REMOTE_BLOG_DIRNAME="/www/static/blog"
-# echo "rm -rf $REMOTE_BLOG_DIRNAME && mkdir -p $REMOTE_BLOG_DIRNAME" | ssh root@$DROPLET_IP
-yarn build
+fnm use
+npm run build
 rsync -r --verbose public $USER@$DROPLET_IP:$REMOTE_BLOG_DIRNAME
+# !!! Run deploy script in provisioner afterwards
