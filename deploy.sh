b8192b22a28969f6bfd7259b88de0231ef89b60b
cdaringe <cdaringe@gmail.com>
2022-03-20 20:16:55 -0700

    refactor: deploy

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ set -euox pipefail
 REMOTE_BLOG_DIRNAME="/www/static/blog"
 fnm use
 npm run build
-rsync -r --verbose public $USER@$DROPLET_IP:$REMOTE_BLOG_DIRNAME
+rsync -r --verbose public $HTTP_SERVER_ADMIN@$HTTP_SERVER_IP:$REMOTE_BLOG_DIRNAME
 # !!! Run deploy script in provisioner afterwards
