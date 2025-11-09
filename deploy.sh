b63b2c54e6f670cb22e5950f865a5b1483276b9a
cdaringe <cdaringe@gmail.com>
2025-11-08 19:35:39 -0800

    chore: go nuts

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 set -euox pipefail
 
+HTTP_SERVER_ADMIN=root
+HTTP_SERVER_IP=$DROPLET_IP
 REMOTE_BLOG_DIRNAME="/www/static/blog"
+SKIP_BUILD=${SKIP_BUILD:-"false"}
+
 fnm use
-npm run build
+if [ "$SKIP_BUILD" = "true" ]; then
+    echo "Skipping build step"
+else
+    npm run build
+fi
 rsync -r --verbose public $HTTP_SERVER_ADMIN@$HTTP_SERVER_IP:$REMOTE_BLOG_DIRNAME
 ssh $HTTP_SERVER_ADMIN@$HTTP_SERVER_IP chown -R $USER:webadmins $REMOTE_BLOG_DIRNAME
