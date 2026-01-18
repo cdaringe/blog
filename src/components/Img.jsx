b5b2371d5d52c5ac077f7cb5c99fcb3774aecf18
cdaringe <cdaringe@gmail.com>
2026-01-18 11:59:33 -0800

    feat: rocker

--- src/components/Img.jsx
+++ src/components/Img.jsx
@@ -4,22 +4,49 @@ import { useFrontmatter } from "./FrontMatterContext";
 
 const DEFAULT_STYLE = { margin: "auto", display: "block", width: "50%" };
 
-const Img = ({ style, idx, ...rest }) => {
+const InternalImg = ({ imageNode, src, imageIdx, style, ...rest }) => {
+  if (imageNode?.childImageSharp) {
+    const imageData = getImage(imageNode);
+    if (!imageData) {
+      throw new Error(`missing childImageSharp data at idx: ${imageIdx}`);
+    }
+    return <GatsbyImage image={imageData} style={style} {...rest} />;
+  }
+  return <img src={imageNode?.publicURL || src} style={style} {...rest} />;
+};
+
+const Img = ({ style, src, idx, clickForFullscreen, ...rest }) => {
   const fm = useFrontmatter();
   const gatsbyImgSrc = fm.embeddedImagesLocal?.[idx];
-  if (!gatsbyImgSrc) {
+  if (!gatsbyImgSrc && !src) {
     throw new Error(`no image found at idx: ${idx}`);
   }
-  if (gatsbyImgSrc.childImageSharp) {
-    <GatsbyImage image={gatsbyImgSrc} style={DEFAULT_STYLE} {...rest} />;
-  }
-  return (
-    <img
-      src={gatsbyImgSrc.publicURL}
-      style={{ ...DEFAULT_STYLE, ...style }}
+
+  const mergedStyle = {
+    ...DEFAULT_STYLE,
+    ...style,
+    ...(clickForFullscreen ? { cursor: "zoom-in" } : {}),
+  };
+
+  const imageEl = (
+    <InternalImg
+      imageNode={gatsbyImgSrc}
+      imageIdx={idx}
+      style={mergedStyle}
+      src={src}
       {...rest}
     />
   );
+
+  if (clickForFullscreen && gatsbyImgSrc?.publicURL) {
+    return (
+      <a href={gatsbyImgSrc.publicURL} style={{ display: "block" }}>
+        {imageEl}
+      </a>
+    );
+  }
+
+  return imageEl;
 };
 
 export default Img;
